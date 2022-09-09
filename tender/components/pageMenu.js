24770c8ca5810ae22454b96417a187516ff47902
Malyushkina <e.malyushkina@mail.ru>
2022-09-09 12:09:06 +0800

    настроила первичные стили

---
+++ tender/components/pageMenu.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Container, Dropdown, Menu } from 'semantic-ui-react';
+import styles from '../styles/menu.module.css';
+
+const PageMenu = () => {
+  return (
+    <Container fluid className={styles.pageMenu}>
+      <Menu size="large" stackable>
+        <Menu.Item>
+          <img className={styles.pageMenu__logo} alt="logo" src="https://react.semantic-ui.com/logo.png" />
+        </Menu.Item>
+      </Menu>
+    </Container>
+  );
+};
+
+export default PageMenu;
